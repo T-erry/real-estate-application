bc9b229c8a3b90accc9a27606d36832efd01b17d
T-erry <terrywachuka10@gmail.com>
2023-08-22 22:20:35 +0300

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Offers from './pages/Offers';
 import './App.css';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
     <>
-      <h1>Hello world!</h1>
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/forgot-password' element={<ForgotPassword/>}/>
+      <Route path='/offers' element={<Offers/>}/>
+    </Routes>
+  </Router>
     </>
   );
 }
