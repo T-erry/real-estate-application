808c6e6f70dc4e3d962df025aade720566d097a3
T-erry <terrywachuka10@gmail.com>
2023-08-23 00:33:47 +0300

    created the header component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Offers from './pages/Offers';
+import Header from './components/Header';
 import './App.css';
 import ForgotPassword from './pages/ForgotPassword';
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
   <Router>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/profile' element={<Profile/>}/>
