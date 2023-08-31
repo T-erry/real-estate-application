fd05e09a2acde0cec9c31007a55794ecf8ce7eb2
T-erry <terrywachuka10@gmail.com>
2023-08-31 13:17:57 +0300

    completed the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react'
 import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword,getAuth } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -10,14 +12,33 @@ export default function SignIn() {
     password: ""
   })
   const {email, password} = formData
+  const navigate = useNavigate();
   
   function onChange (e){
     
     setFormData((prevState)=>({
-      ...prevState, [e.target.id]: [e.target.value]
+      ...prevState, [e.target.id]: e.target.value
     } ))
     
   }
+
+ async function handleSubmit(e){
+  e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      if(userCredential.user){
+        navigate('/')
+      }
+      
+    } catch (error) {
+      toast.error("Bad user credentials")
+      
+    }
+
+  }
+
+
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
@@ -27,7 +48,7 @@ export default function SignIn() {
           </div>
 
       <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-        <form >
+        <form onSubmit={handleSubmit} >
           <input  className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6' type='email' id="email" value={email} onChange={onChange} placeholder= 'Email address'/>
           <div className='relative mb-6'>
           <input  className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type={showPassword ? "text" : "password"} id="password" value={password} onChange={onChange} placeholder= 'Password'/>
